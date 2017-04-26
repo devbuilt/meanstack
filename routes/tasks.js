67c87002b1f9dfb336a648134b12a9f1c68948bc
Leonel Tejeda <leonelhtejeda@gmail.com>
2017-04-25 21:04:11 -0700

    angular4

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://leo:Lt171392@ds117251.mlab.com:17251/website_leonel',['tasks']);
+var db = mongojs('mongodb://leo:Lt171392@ds117251.mlab.com:17251/website_leonel', ['tasks']);
 
 //Get All Tasks
 router.get('/tasks', function(req, res, next){
@@ -42,13 +42,44 @@ router.post('/task', function(req, res, next){
 });
 
 //Delete Task
-router.get('/tasks', function(req, res, next){
-	db.tasks.find(function(err, tasks){
+router.delete('/task/:id', function(req, res, next){
+	db.tasks.remove(function(err, tasks){
 		if(err){
 			res.send(err);
 		}
 		res.json(tasks);
 	});
+});
+
+//Update Task
+router.put('/task/:id', function(req, res, next){
+	var task = req.body;
+	var updTask = {}
+	
+	if(task.isDone){
+		updTask.isDone = task.isDone;
+	}
+	if(task.title){
+		updTask.title = task.title;
+	}	
+	
+	if(!updTask){
+		res.status(400);
+		res.json({
+			"error": "Bad Data"
+		});
+		
+	}else{
+		db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, updTask, {}, function(err, task){
+		if(err){
+			res.send(err);
+		}
+		res.json(task);
+	
+		});	
+	
+	}
+	
 });
 
 module.exports = router;
